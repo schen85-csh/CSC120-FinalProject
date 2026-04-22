628c2efc2b5605097d0a147d717b89f035c7499a
schen85-csh <schen85@smith.edu>
2026-04-21 22:38:34 -0400

    finish basic room class

--- Room.java
+++ Room.java
@@ -1,5 +1,35 @@
+import java.util.HashMap;
+import java.util.List;
 public class Room {
     private String name;
     private String description;
-    
+    private HashMap<String, Room> exits;
+    private List<Item> items;
+    private List<Zombie> zombies;
+
+    public void setExit(String direction, Room neighbor){
+        exits.put(direction, neighbor);
+    }
+
+    public Room getExit(String direction){
+        return exits.get(direction);
+    }
+
+    public void addItem(Item item){
+        items.add(item);
+    }
+
+    public Item removeItem(String itemName){
+        for (Item i:items){
+            if(i.getName().contains(itemName)){
+                items.remove(i);
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public String getDescription(){
+        return "Location: " + name + "\n" + description; 
+    }
 }
