2127f0c177a7aa28ccf60228177e00999e6e10b7
schen85-csh <schen85@smith.edu>
2026-04-21 23:02:28 -0400

    basic code for buildingStructure file

---
+++ BuildingStructure.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.Map;
+
+public class BuildingStructure{
+
+    private Map<String, Room> rooms;
+    private Room currentRoom;
+    private Room startRoom;
+
+   // I haven't finish designing the whole structure. I will update the file before Thursday.
+}
